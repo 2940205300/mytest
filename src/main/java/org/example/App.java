3cdc6be410082b4621be483431dc2eff62912e19
denghuan <2940205300@qq.com.com>
2023-06-11 23:54:02 +0800

    v4.0.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World jenkins---v3.0.0");
-        System.out.println("Hello World jenkins---v3.0.0");
+        System.out.println("Hello World jenkins---v4.0.0  这是第4版本");
+
     }
 }
